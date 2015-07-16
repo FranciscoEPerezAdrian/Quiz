ea4d24d4d713a849715cb19356a39a0211893d90
FranciscoEPerezAdrian <franciscoeperez77@gmail.com>
2015-07-16 18:36:42 +0200

    Lista de Preguntas

--- routes/index.js
+++ routes/index.js
@@ -8,8 +8,15 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
 
-router.get('/quizes/question', quizController.question);
-router.get('/quizes/answer', quizController.answer);
+//Rutas antiguas cuando había una sola pregunta
+//router.get('/quizes/question', quizController.question);
+//router.get('/quizes/answer', quizController.answer);
+
+//Rutas para múltiples preguntas
+//Definición de rutas de /quizes
+router.get('/quizes',						quizController.index);
+router.get('/quizes/:quizId(\\d+)',			quizController.show);
+router.get('/quizes/:quizId(\\d+)/answer',	quizController.answer);
 
 /* GET author, no se incluye en controller por no tener lógica asociada */
 router.get('/author', function(req, res){
