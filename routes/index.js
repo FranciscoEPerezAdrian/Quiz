df40b8a1aaaa878e728f1420bc436217b2d98d69
FranciscoEPerezAdrian <franciscoeperez77@gmail.com>
2015-07-18 01:39:01 +0200

    Crear Preguntas

--- routes/index.js
+++ routes/index.js
@@ -16,6 +16,8 @@ router.param('quizId', quizController.load);  //autoload :quizId
 router.get('/quizes',						quizController.index);
 router.get('/quizes/:quizId(\\d+)',			quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer',	quizController.answer);
+router.get('/quizes/new',					quizController.new);
+router.post('/quizes/create',				quizController.create);
 
 /* GET author, no se incluye en controller por no tener lógica asociada */
 router.get('/author', function(req, res){
