0896077b25cd7a2dd0d9dc0796a275a017ee0c92
FranciscoEPerezAdrian <franciscoeperez77@gmail.com>
2015-07-18 03:13:44 +0200

    Validacion de Entradas

--- models/quiz.js
+++ models/quiz.js
@@ -1,8 +1,16 @@
-// Definicion del modelo de Quiz
+// Definicion del modelo de Quiz con validación
 
 module.exports = function(sequielice, DataTypes){
-	return sequielice.define('Quiz',
-				{pregunta:  DataTypes.STRING,
-				 respuesta: DataTypes.STRING,
-				});
+	return sequielice.define(
+		'Quiz',
+		{ pregunta: {
+			type: DataTypes.STRING,
+			validate: { notEmpty: {msg: "-> Falta Pregunta"}}
+		   },  
+		   respuesta: {
+				type: DataTypes.STRING,
+				validate: {notEmpty: {msg: "-> Falta Respuesta"}}
+			} 
+		}
+	);
 }
