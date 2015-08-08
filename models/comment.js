95cabecb343d7c9f4316536c57b1947b6734760d
FranciscoEPerezAdrian <franciscoeperez77@gmail.com>
2015-08-08 17:14:40 +0200

    Moderacion de Comentarios

--- models/comment.js
+++ models/comment.js
@@ -6,7 +6,11 @@ module.exports = function(sequielize, DataTypes) {
 		{ texto: {
 			type: DataTypes.STRING,
 			validate: { notEmpty: {msg: "-> Falta Comentario"}}
+		   },
+		   publicado: {
+		   		type: DataTypes.BOOLEAN,
+		   		defaultValue: false
+		   }
 		}
-	}
 	);
 }
